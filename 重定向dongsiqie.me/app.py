55ef59978131eb0cbcd68fb1a1447719f4876c17
pengqi <104043801+yangjianchuan@users.noreply.github.com>
2024-02-09 16:05:14 +0800

    Update app.py

--- 重定向dongsiqie.me/app.py
+++ 重定向dongsiqie.me/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return redirect('https://dongsiqie.me', code=302)
 
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def redirect_all(path):
+    return redirect('https://dongsiqie.me', code=302)
+
 if __name__ == '__main__':
     app.run()
